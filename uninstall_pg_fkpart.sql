bc880f9136c9a548ff616e05a0cbea9b0fb0d592
Nicolas Relange <nrelange@lemoinetechnologies.com>
2015-04-27 17:12:34 +0200

    Missing copyright information

--- uninstall_pg_fkpart.sql
+++ uninstall_pg_fkpart.sql
@@ -7,6 +7,7 @@
 -- PostgreSQL Partitioning Utility
 --
 -- Copyright(C) 2012 Uptime Technologies, LLC.
+-- Copyright(C) 2013 Lemoine Automation Technologies
 --
 -- This program is free software; you can redistribute it and/or modify
 -- it under the terms of the GNU General Public License as published by
