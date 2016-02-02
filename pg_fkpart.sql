9079127f1bd28b132151b129563cb830cb3fb991
Nicolas Relange <nrelange@lemoinetechnologies.com>
2016-02-02 10:02:22 +0100

    Add the version number in pg_fkpart.sql

--- pg_fkpart.sql
+++ pg_fkpart.sql
@@ -17,6 +17,10 @@
 -- with this program; if not, write to the Free Software Foundation, Inc.,
 -- 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 --
+
+-- pg_fkpart unpackaged (1.3.0)
+
+-- Comment the next line for an upgrade
 CREATE SCHEMA pgfkpart;
 
 -- TODO:
